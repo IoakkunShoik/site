10c9569ff36437bf85dced21cd839e4f3a67f873
IoakkunShoik <ioa.sh99@gmail.com>
2019-11-02 21:34:36 +0300

    полная реализация get в разделе controller

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -1,12 +1,26 @@
 <?php
 namespace root\controller;
 require_once 'view/instructions.php';
+
 class main_controller{
-    function __construct(){ 
-        echo "скрипт подгружен";
-        echo "<hr>";
-        $page = new \root\view\view();
-        $page->render('1232');
+    private function page_index($page){
+        return $page->render('это главная страница');
+    }
+    private function page_shop($page){
+        return $page->render('Это страница магазина');
+    }
+
+    function __construct(){
+        $page = new \root\view\view();         
+        switch ($_GET["page"])
+        {
+            case 'index': $this->page_index($page);break;
+            case  'shop': $this->page_shop($page);break;
+            default:{echo $_GET['page'];
+                $this->page_index();}
+            
+        }
+        
     }
 
 }
--- index.php
+++ index.php
@@ -2,9 +2,4 @@
 
 require_once 'controller/main_controller.php';
 
-class main{
-    function __construct(){
-        
-    }
-}
 $a = new \root\controller\main_controller();        //подключить контроллер
